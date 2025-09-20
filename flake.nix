f03fab2a2b69339006bedb8962cba7ee0d9b8e74
William Jelgren <william@jelgren.dk>
2025-09-20 19:07:30 +0200

    flake: Small change to flake

--- flake.nix
+++ flake.nix
@@ -12,11 +12,41 @@
     eachSystem = function:
       nixpkgs.lib.genAttrs systems
       (system: function nixpkgs.legacyPackages.${system});
+
+    jdkVersion = "21";
   in {
+    packages = eachSystem (
+      pkgs: let
+        jdk = pkgs."jdk${jdkVersion}";
+        maven = pkgs.maven.override {jdk_headless = jdk;};
+        pname = "socket-chat-server";
+        jarName = "ChatProgramSockets-0.1.0";
+      in {
+        server = pkgs.maven.buildMavenPackage rec {
+          inherit pname;
+          version = "0.1.0";
+          src = ./.;
+
+          mvnParameters = "-Pclient";
+
+          mvnHash = "sha256-vd1EqcRJlb8onwBbZ/oDxDoL/oPUwGPJFrcHzQpyilY=";
+
+          nativeBuildInputs = [pkgs.makeWrapper];
+
+          installPhase = ''
+            mkdir -p $out/bin $out/share/${pname}
+            install -Dm644 target/${jarName}.jar $out/share/${pname}
+
+            makeWrapper ${jdk}/bin/java $out/bin/${pname} \
+              --add-flags "-jar $out/share/${pname}/${jarName}.jar"
+          '';
+        };
+      }
+    );
+
     devShells = eachSystem (
       pkgs: let
-        jdk-version = "21";
-        jdk = pkgs."jdk${jdk-version}";
+        jdk = pkgs."jdk${jdkVersion}";
 
         maven = pkgs.maven.override {jdk_headless = jdk;};
         gradle = pkgs.gradle.override {java = jdk;};
